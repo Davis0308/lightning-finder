9591be20b16940b077132d6d92429dd5e6d8d8ef
Davis Schina <davisschina@gmail.com>
2023-07-28 17:57:22 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,4 @@
 class settings:
     delete_proc_dir_when_done = True #choose whether to delete the processing folder once the script finishes [default: 1]
     proc_dir_name = "processing" #choose name of processing dir [default: processing]
-    video_file_name = "video.mp4"
-    
+    video_file_name = "video.mp4" #name of the video file to process in the root folder [default: video.mp4]
