9b8a0ec88c66dbdf52c2ce687ea4ef202c3ce839
Davis Schina <davisschina@gmail.com>
2023-07-29 01:09:52 +0200

    changed to f strings, fixed caps, improved variables

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+# ruff: noqa: E501
 import os
 import shutil
 from PIL import Image
@@ -5,13 +6,13 @@ from PIL import ImageStat
 import math
 
 #remove files from a folder only if the folder exists
-def clear_dir_only_if_exists(directory_to_remove, directory_name):
+def clear_dir_only_if_exists(directory_to_remove, directory_name, current_directory):
     if os.path.exists(directory_to_remove) is True:
         shutil.rmtree(directory_to_remove)
         os.mkdir(directory_to_remove)
-        print(f"contents of the folder .\\{directory_name} deleted successfully\n")
+        print(f"\nContents of the folder .\\{current_directory}\\{directory_name} deleted successfully.\n")
     else:
-        print(f"the folder .\\{directory_name} does not exist anymore. "
+        print(f"The folder .\\{current_directory}\\{directory_name} does not exist anymore. "
               "Creating it and continuing.\n")
         os.mkdir(directory_to_remove)
 
--- main.py
+++ main.py
@@ -24,29 +24,33 @@ print("lightning-finder by Davis0308 - github.com/Davis0308/lightning-finder - G
 
 #defining current working directory
 cwd = Path.cwd()
+cwd_name = "lightning-finder"
 
 #defining processing folder path
 processing_dir_name = config.settings.proc_dir_name
 processing_dir = cwd / processing_dir_name
 
+#defining video file name
+video_file_name = config.settings.video_file_name
+
 #checking if processing folder doesn't exist; if it doesn't, create it
-print("checking for the existance of processing folder...")
+print("Checking for the existance of processing folder...")
 if os.path.exists(processing_dir) is False:
     os.mkdir(processing_dir)
-    print(f"processing folder named {processing_dir_name} created\n")
+    print(f"Processing folder named {processing_dir_name} created.\n")
 else:
-    print("processing folder already exists\n")
+    print("Processing folder already exists.\n")
 
 #checking if processing folder is empty; if not, ask to delete items
 if len(os.listdir(processing_dir)) != 0:
-    proc_empty_input = input("WARNING:\n"
-    "The processing folder is not empty. "
-    "To continue, the contents of the folder need to be deleted. "
+    print("\033[91mWARNING:\033[0m")
+    proc_empty_input = input(f"The processing folder located at .\\{cwd_name}\\{processing_dir_name} is not empty.\n"
+    "To continue, the contents of the folder need to be deleted.\n"
     "Please check that you don't have any important files in the "
-    "folder as they will be removed. "
+    "folder as they will be removed.\n"
     "Would you like to continue?\n(yes/No): ").lower()
     if proc_empty_input == "yes":
-        functions.clear_dir_only_if_exists(processing_dir, processing_dir_name)
+        functions.clear_dir_only_if_exists(processing_dir, processing_dir_name, cwd_name)
     else:
         exit()
 
@@ -57,10 +61,10 @@ ffmpeg_output = f"{processing_dir}/%d.png"
 subprocess.run(["ffmpeg","-i",config.settings.video_file_name,ffmpeg_output])
 
 #getting video's FPS
-fpsfind = cv2.VideoCapture("video.mp4")
+fpsfind = cv2.VideoCapture(video_file_name)
 fps = fpsfind.get(cv2.CAP_PROP_FPS)
 fps = round(fps, 3)
-print("\n\n\n" + "fps: " + str(fps))
+print(f"\nFps: {fps}")
 
 #creating final array for brightness values
 brightness_array = []
@@ -71,7 +75,7 @@ frame_count = 0
 for path in os.scandir(processing_dir):
     if path.is_file():
         frame_count += 1
-print("number of frames: " + str(frame_count))
+print(f"Number of frames: {frame_count}")
 
 #listing methods for easy switching while testing
 bf1 = functions.get_average_perceived_brightness
@@ -96,26 +100,26 @@ for n in range(frame_count):
     timestamp_array.append(timestamp_in_s)
 
 #creating plotted graph
-print("\nbrightness data points: " + str(len(brightness_array)))
-print("timestamp data points: " + str(len(timestamp_array)))
+print(f"\nBrightness data points acquired: {len(brightness_array)}")
+print(f"Timestamp data points acquired: {len(timestamp_array)}")
 plt.plot(timestamp_array, brightness_array)
 
 #deleting contents of processing folder
 if config.settings.delete_proc_dir_when_done is True:
-    nofipf = len(os.listdir(processing_dir)) #getting num. of files in processing folder
+    number_of_files_in_processing_folder = len(os.listdir(processing_dir)) #getting num. of files in processing folder
     shutil.rmtree(processing_dir)
-    print(f"files removed in .\\{processing_dir_name}: {nofipf}")
+    print(f"{number_of_files_in_processing_folder} files removed in .\\{cwd_name}\\{processing_dir_name}")
 
 #ending timers
 timer_end_performance = time.perf_counter()
 time_elapsed_performance = (timer_end_performance-timer_start_performance)
 time_elapsed_performance = round(time_elapsed_performance, 2)
-print(f"\n\nFinished\nScript execution took {str(time_elapsed_performance)} seconds")
+print(f"\n\nFinished.\nScript execution took {time_elapsed_performance} seconds")
 
 timer_end_process = time.process_time()
 time_elapsed_process = (timer_end_process-timer_start_process)
 time_elapsed_process = round(time_elapsed_process, 2)
-print(f"Process took {str(time_elapsed_process)} seconds\n")
+print(f"Process took {time_elapsed_process} seconds\n")
 
 #showing plot
 plt.show()
