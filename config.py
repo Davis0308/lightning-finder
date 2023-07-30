77dd5b16826dbabd93f587778e0391bed3c4f027
Davis Schina <davisschina@gmail.com>
2023-07-30 03:08:44 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ class MainSettings:
     delete_proc_dir_when_done = True #choose whether to delete the processing folder once the script finishes [default: True]
     proc_dir_name = "processing" #choose name of processing dir [default: processing]
     video_file_name = "video.mp4" #name of the video file to process in the root folder [default: video.mp4]
-    custom_frameres = False #choose if the frames should be rendered at the same resolution as they are in the video or if you'd like to change frame resolution in frame_res [default: False]
+    custom_frameres = False #choose if the frames should be rendered at the same resolution as they are in the video (False) or if you'd like to change frame resolution in frame_res (True) [default: False]
     frame_res = "1920x1080" # WidthxHeight [default: 1920x1080]
     average_brightness_algo = 2 #choose the brightness algorithm, from 0 to 6 (check functions.py) [default: 2]
